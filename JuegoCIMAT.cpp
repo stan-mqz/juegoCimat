653f5f561467b9951574f8127d9ba780c0b9bd23
stan-mqz <moscarstanley37@gmail.com>
2024-07-16 08:19:15 -0600

    Version totalmente funcional

--- JuegoCIMAT.cpp
+++ JuegoCIMAT.cpp
@@ -15,8 +15,8 @@ int main()
     {
         cout << "Seleccione la dificultad" << "\n";
         cout << "0. Salir" << "\n";
-        cout << "1. Fácil" << "\n";
-        cout << "2. Difícil" << "\n";
+        cout << "1. Facil" << "\n";
+        cout << "2. Dificil" << "\n";
         cin >> dificultad;
 
         if (dificultad == 0)
@@ -31,7 +31,7 @@ int main()
         }
         else
         {
-            cout << "Ingrese una opción válida" << "\n";
+            cout << "Ingrese una opcion valida" << "\n";
         }
     } // ciclo while de opciones
 
@@ -44,7 +44,7 @@ int main()
     if (dificultad == 1)
     {
         cout << "======================================" << "\n";
-        cout << "Ha seleccionado la dificultad: Fácil" << "\n";
+        cout << "Ha seleccionado la dificultad Facil" << "\n";
         cout << "======================================" << "\n";
         cin.ignore();
         cin.get();
@@ -72,140 +72,140 @@ int main()
                 {
 
                 case 1:
-                    cout << "¿Cuánto es 2 + 2?" << "\n";
+                    cout << "Cuanto es 2 + 2?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[0] << endl;
                     break;
 
                 case 2:
-                    cout << "¿Cuánto es 10/5?" << "\n";
+                    cout << "Cuanto es 10/5?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[1] << endl;
                     break;
 
                 case 3:
-                    cout << "¿Cuánto es 9 + 10?" << "\n";
+                    cout << "Cuanto es 9 + 10?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[2] << endl;
                     break;
 
                 case 4:
-                    cout << "¿Cuánto es 100/5?" << "\n";
+                    cout << "Cuanto es 100/5?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[3] << endl;
                     break;
 
                 case 5:
-                    cout << "¿Cuánto es 5 x 10?" << "\n";
+                    cout << "Cuanto es 5 x 10?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[4] << endl;
                     break;
 
                 case 6:
-                    cout << "¿Cuánto es 9 x 9?" << "\n";
+                    cout << "Cuanto es 9 x 9?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[5] << endl;
                     break;
 
                 case 7:
-                    cout << "¿Cuánto es 50/2?" << "\n";
+                    cout << "Cuanto es 50/2?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[6] << endl;
                     break;
 
                 case 8:
-                    cout << "¿Cuánto es 200 + 500?" << "\n";
+                    cout << "Cuanto es 200 + 500?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[7] << endl;
                     break;
 
                 case 9:
-                    cout << "¿Cuánto es 100/20?" << "\n";
+                    cout << "Cuanto es 100/20?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[8] << endl;
                     break;
 
                 case 10:
-                    cout << "¿Cuánto es 7 x 3?" << "\n";
+                    cout << "Cuanto es 7 x 3?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[9] << endl;
                     break;
 
                 case 11:
-                    cout << "¿Cuánto es 15 - 10?" << "\n";
+                    cout << "Cuanto es 15 - 10?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[10] << endl;
                     break;
 
                 case 12:
-                    cout << "¿Cuánto es 2 + 12?" << "\n";
+                    cout << "Cuanto es 2 + 12?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[11] << endl;
                     break;
 
                 case 13:
-                    cout << "¿Cuánto es 30 - 15?" << "\n";
+                    cout << "Cuanto es 30 - 15?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[12] << endl;
                     break;
 
                 case 14:
-                    cout << "¿Cuánto es 50 + 50?" << "\n";
+                    cout << "Cuanto es 50 + 50?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[13] << endl;
                     break;
 
                 case 15:
-                    cout << "¿Cuánto es 200 - 90?" << "\n";
+                    cout << "Cuanto es 200 - 90?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[14] << endl;
                     break;
 
                 case 16:
-                    cout << "¿Cuánto es 8 x 9?" << "\n";
+                    cout << "Cuanto es 8 x 9?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[15] << endl;
                     break;
 
                 case 17:
-                    cout << "¿Cuánto es 4 x 4?" << "\n";
+                    cout << "Cuanto es 4 x 4?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[16] << endl;
                     break;
 
                 case 18:
-                    cout << "¿Cuánto es 70 - 35?" << "\n";
+                    cout << "Cuanto es 70 - 35?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[17] << endl;
                     break;
 
                 case 19:
-                    cout << "¿Cuánto es 60 + 15?" << "\n";
+                    cout << "Cuanto es 60 + 15?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[18] << endl;
                     break;
 
                 case 20:
-                    cout << "¿Cuánto es 300/2?" << "\n";
+                    cout << "Cuanto es 300/2?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestas[19] << endl;
@@ -216,7 +216,7 @@ int main()
                     break;
 
                 } // Switch de preguntas
-            } // if que define el número de la pregunta
+            } // if que define el numero de la pregunta
             else
             {
                 cout << "Ha salido del juego. Gracias por jugar" << "\n";
@@ -229,7 +229,7 @@ int main()
     if (dificultad == 2)
     {
         cout << "=======================================" << "\n";
-        cout << "Ha seleccionado la dificultad: Difícil" << "\n";
+        cout << "Ha seleccionado la dificultad Dificil" << "\n";
         cout << "=======================================" << "\n";
         cin.ignore();
         cin.get();
@@ -257,140 +257,140 @@ int main()
                 {
 
                 case 1:
-                    cout << "¿Cuánto es 4 × 4 - 4 + 4 x 4?" << "\n";
+                    cout << "Cuanto es 4 x 4 - 4 + 4 x 4?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[0] << endl;
                     break;
 
                 case 2:
-                    cout << "¿Cuánto es 132/4?" << "\n";
+                    cout << "Cuanto es 132/4?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[1] << endl;
                     break;
 
                 case 3:
-                    cout << "¿Cuánto es 160/0.25?" << "\n";
+                    cout << "Cuanto es 160/0.25?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[2] << endl;
                     break;
 
                 case 4:
-                    cout << "¿Cuánto es 48.25 - 33.25?" << "\n";
+                    cout << "Cuanto es 48.25 - 33.25?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[3] << endl;
                     break;
 
                 case 5:
-                    cout << "¿Cuánto es 19 x 20?" << "\n";
+                    cout << "Cuanto es 19 x 20?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[4] << endl;
                     break;
 
                 case 6:
-                    cout << "¿Cuánto es 36 x 4?" << "\n";
+                    cout << "Cuanto es 36 x 4?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[5] << endl;
                     break;
 
                 case 7:
-                    cout << "¿Cuánto es 440 x 20?" << "\n";
+                    cout << "Cuanto es 440 x 20?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[6] << endl;
                     break;
 
                 case 8:
-                    cout << "¿Cuánto es 120 x 0.25?" << "\n";
+                    cout << "Cuanto es 120 x 0.25?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[7] << endl;
                     break;
 
                 case 9:
-                    cout << "¿Cuánto es 648/24?" << "\n";
+                    cout << "Cuanto es 648/24?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[8] << endl;
                     break;
 
                 case 10:
-                    cout << "¿Cuánto es 840 - 390?" << "\n";
+                    cout << "Cuanto es 840 - 390?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[9] << endl;
                     break;
 
                 case 11:
-                    cout << "¿Cuánto es 0.3% de 100?" << "\n";
+                    cout << "Cuanto es 0.3% de 100?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[10] << endl;
                     break;
 
                 case 12:
-                    cout << "¿Cuánto es 496/4?" << "\n";
+                    cout << "Cuanto es 496/4?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[11] << endl;
                     break;
 
                 case 13:
-                    cout << "¿Cuánto es 840/30?" << "\n";
+                    cout << "Cuanto es 840/30?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[12] << endl;
                     break;
 
                 case 14:
-                    cout << "¿Cuánto es el 75% de 804?" << "\n";
+                    cout << "Cuanto es el 75% de 804?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[13] << endl;
                     break;
 
                 case 15:
-                    cout << "¿Cuánto es 100/0.25?" << "\n";
+                    cout << "Cuanto es 100/0.25?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[14] << endl;
                     break;
 
                 case 16:
-                    cout << "¿Cuánto es 440/4?" << "\n";
+                    cout << "Cuanto es 440/4?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[15] << endl;
                     break;
 
                 case 17:
-                    cout << "¿Cuánto es 25% de 1636?" << "\n";
+                    cout << "Cuanto es 25% de 1636?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[16] << endl;
                     break;
 
                 case 18:
-                    cout << "¿Cuánto es 2220/20?" << "\n";
+                    cout << "Cuanto es 2220/20?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[17] << endl;
                     break;
 
                 case 19:
-                    cout << "Lisa recibió un cheque regalo de 100 euros por su cumpleaños. Se compró unas deportivas que costaban 30 euros, un vestido de 23 euros y dos libros de 17 euros. ¿Cuánto dinero le quedó en el cheque regalo?" << "\n";
+                    cout << "Lisa recibio un cheque regalo de 100 euros por su cumpleanos. Se compro unas deportivas que costaban 30 euros, un vestido de 23 euros y dos libros de 17 euros. Cuanto dinero le quedo en el cheque regalo?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[18] << endl;
                     break;
 
                 case 20:
-                    cout << "Si David tiene el doble de monedas de 5 céntimos que Tomás y Tomás tiene 15 monedas de 5 céntimos más que Juan, ¿cuántas monedas tiene David si Juan tiene 6 monedas de cinco céntimos?" << "\n";
+                    cout << "Si David tiene el doble de monedas de 5 centimos que Tomas y Tomas tiene 15 monedas de 5 centimos mas que Juan, cuantas monedas tiene David si Juan tiene 6 monedas de cinco centimos?" << "\n";
                     cin.ignore();
                     cin.get();
                     cout << respuestasDificiles[19] << endl;
@@ -401,7 +401,7 @@ int main()
                     break;
 
                 } // Switch de preguntas
-            } // if que define el número de la pregunta
+            } // if que define el numero de la pregunta
             else
             {
                 cout << "Ha salido del juego. Gracias por jugar" << "\n";
